1dffbaf98deeda5583a327102e3e22336cdf1746
kms-loantran <loan.tran@katalon.com>
2023-05-18 13:48:46 +0700

    Update Script1609735512288.groovy

--- Scripts/webui - screenshot/Script1609735512288.groovy
+++ Scripts/webui - screenshot/Script1609735512288.groovy
@@ -13,7 +13,6 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 import java.awt.Color
 import org.openqa.selenium.Rectangle
