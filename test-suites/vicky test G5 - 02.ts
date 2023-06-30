01420be6eedd547dff1f637eafa04ae2c63cc694
kms-loantran <loan.tran@katalon.com>
2023-06-30 16:37:48 +0700

    test

---
+++ test-suites/vicky test G5 - 02.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>vicky test G5 - 02</name>
+    <testSuiteGuid>554bf708-744f-4a39-af89-ba7ab248e4ab</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/vicky test G5 - 02</testCaseId>
+        <guid>a1badf9a-5689-4084-8dcf-f2a77fbe6bda</guid>
+    </testCaseLink>
+</TestSuiteEntity>
