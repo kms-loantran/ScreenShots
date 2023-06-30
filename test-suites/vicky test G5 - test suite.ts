742feeda064eefde41b6ccccdcd0b3f00fb9adad
kms-loantran <loan.tran@katalon.com>
2023-06-30 15:55:49 +0700

    test G5 suite

---
+++ test-suites/vicky test G5 - test suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>vicky test G5 - test suite</name>
+    <testSuiteGuid>a6b18cd0-b7d4-446a-9ec2-5e7c909230e2</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/vicky test G5 - 01</testCaseId>
+        <guid>4df486fe-4cf0-4cff-a9e5-15bd63881a41</guid>
+    </testCaseLink>
+</TestSuiteEntity>
